a462bab26ced82a4ca284c4d2f96a5ba35e762da
Sheetal Atre <sheetal.atre@opsmx.io>
2023-05-22 13:49:02 +0530

    Update HelmAccounts.java
    replaced SpinnakerException with SpinnakerServerException

--- igor-web/src/main/java/com/netflix/spinnaker/igor/helm/accounts/HelmAccounts.java
+++ igor-web/src/main/java/com/netflix/spinnaker/igor/helm/accounts/HelmAccounts.java
@@ -63,7 +63,7 @@ public class HelmAccounts {
               .filter(it -> it.types.contains("helm/chart"))
               .map(it -> new HelmAccount(it.name))
               .collect(Collectors.toList());
-    } catch (SpinnakerException e) {
+    } catch (SpinnakerServerException e) {
       log.error("Failed to get list of Helm accounts", e);
     } catch (RetrofitError e) {
       log.error("Failed to get list of Helm accounts", e);
